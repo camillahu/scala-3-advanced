5c0955030f65e0683acee9e6b84fafe19e524901
Camilla H. Urdal <camillahaugenurdal@gmail.com>
2025-01-22 15:22:20 +0100

    Update LzList.scala

--- src/main/scala/com/rockthejvm/practise/LzList.scala
+++ src/main/scala/com/rockthejvm/practise/LzList.scala
@@ -1,6 +1,5 @@
 package com.rockthejvm.practise
 
-import javax.sql.rowset.Predicate
 import scala.annotation.tailrec
 
 //Write a lazily evaluated, potentially INFINITE linked list
@@ -11,7 +10,7 @@ abstract class LzList[A] {
 
   //utils
   infix def #::(element: A): LzList[A] //prepending
-  infix def ++(another: LzList[A]): LzList[A] // TODO warning
+  infix def ++(another: => LzList[A]): LzList[A]
 
   //classics
   def foreach(f: A => Unit):Unit
@@ -21,8 +20,15 @@ abstract class LzList[A] {
   def withFilter(predicate: A => Boolean): LzList[A] = filter(predicate)
 
   def take(n:Int): LzList[A] //takes first n elements from this lazy list
-  def takeAsList(n:Int): List[A]
-  def toList: List[A] //use this carefully, can overflow
+  def takeAsList(n:Int): List[A] = take(n).toList
+  def toList: List[A] = {
+    @tailrec
+    def toListAux(remaining: LzList[A], aList: List[A]): List[A] = {
+      if(remaining.isEmpty) aList.reverse
+      else toListAux(remaining.tail, remaining.head :: aList)
+    }
+    toListAux(this, List.empty)
+  } //use this carefully, can overflow
 }
 
 case class LzEmpty[A]() extends LzList[A] {
@@ -33,42 +39,47 @@ case class LzEmpty[A]() extends LzList[A] {
   def tail: LzList[A] = throw new NoSuchElementException("Empty LzList has no tail")
 
   //utils
-  infix def #::(element: A): LzList[A] = new LzCons[A](element, LzEmpty())
+  infix def #::(element: A): LzList[A] = new LzCons[A](element, this)
 
-  infix def ++(another: LzList[A]): LzList[A] = another
+  infix def ++(another: => LzList[A]): LzList[A] = another
 
   //classics
   def foreach(f: A => Unit): Unit = ()
 
-  def map[B](f: A => B): LzList[B] = LzEmpty[B]()
+  def map[B](f: A => B): LzList[B] = LzEmpty()
 
-  def flatMap[B](f: A => LzList[B]): LzList[B] = LzEmpty[B]()
+  def flatMap[B](f: A => LzList[B]): LzList[B] = LzEmpty()
 
   def filter(predicate: A => Boolean): LzList[A] = this
 
-  def take(n: Int): LzList[A] = throw new UnsupportedOperationException("Cannot take from Empty LzList")
-
-  def takeAsList(n: Int): List[A] = throw new UnsupportedOperationException("Cannot take from Empty LzList")
-
-  def toList: List[A] = List()
+  def take(n: Int): LzList[A] = {
+    if (n == 0) this
+    else throw new RuntimeException(s"cannot taks $n elements from an empty lazy list.")
+  }
 }
 
 class LzCons[A](hd: => A, tl: => LzList[A]) extends LzList[A] {
   def isEmpty: Boolean = false
 
-  lazy val head: A = hd
+  override lazy val head: A = hd
 
-  lazy val tail: LzList[A] = tl
+  override lazy val tail: LzList[A] = tl
 
   //utils
   infix def #::(element: A): LzList[A] = new LzCons[A](element, this)
 
-  infix def ++(another: LzList[A]): LzList[A] = new LzCons(this.head, this.tail++another)
+  infix def ++(another: => LzList[A]): LzList[A] = new LzCons(this.head, this.tail++another)
 
   //classics
   def foreach(f: A => Unit): Unit = {
-    f(this.head)
-    tail.foreach(f)
+    @tailrec
+    def foreachTailRec(lzList: LzList[A]): Unit =
+      if (lzList.isEmpty) ()
+      else {
+        f(lzList.head)
+        foreachTailRec(lzList.tail)
+      }
+    foreachTailRec(this)
   }
 
   def map[B](f: A => B): LzList[B] = {
@@ -76,39 +87,66 @@ class LzCons[A](hd: => A, tl: => LzList[A]) extends LzList[A] {
   }
 
   def flatMap[B](f: A => LzList[B]): LzList[B] = {
-    f(head) ++ tail.flatMap(f)
+    f(head) ++ tail.flatMap(f) //preserves lazy evaluation
   }
 
   def filter(predicate: A => Boolean): LzList[A] = {
-    if(!predicate(head)) tail.filter(predicate)
-    else head #:: tail.filter(predicate)
+    // not using head #:: tail.filter(predicate) because it's not lazy
+    if(predicate(head)) new LzCons(head, tail.filter(predicate))
+    else tail.filter(predicate)
   }
 
   def take(n: Int): LzList[A] = {
-      if (n == 0) this
-      else this.head #:: tail.take(n-1)
+      if (n <= 0) LzEmpty()
+      else if (n == 1) new LzCons(head, LzEmpty())
+      // not using head #:: tail.take(n-1) because it's not lazy
+      else new LzCons(head, tail.take(n - 1))
   }
-
-  def takeAsList(n: Int): List[A] = {
-    @tailrec
-    def collect(lz:LzList[A], count:Int, aList:List[A]):List[A] = {
-      if(count == 0) aList
-      else collect(lz.tail, count-1, lz.head :: aList)
-    }
-    collect(this.take(n), n, List.empty)
-  }
- 
-
-  def toList: List[A] =  ???//use this carefully, can overflow
 }
 
+
 object LzList {
-  def generate[A](start: A)(generator: A => A): LzList[A] = ???
-  def from[A](list: List[A]): LzList[A] = ???
+  def empty[A]: LzList[A] = LzEmpty()
+
+  def generate[A](start: A)(generator: A => A): LzList[A] = {
+    new LzCons(start, LzList.generate(generator(start))(generator))
+  }
+
+  def from[A](list: List[A]): LzList[A] = list.reverse.foldLeft(LzList.empty) { (currentLzList, newElement) =>
+    new LzCons(newElement, currentLzList)
+  }
+
+  def apply[A](values: A*) = LzList.from(values.toList)
+
+  def fibonacci[Int](): LzList[Int] = {
+    LzList.generate(1){(newFib) => 
+      
+    }
+  }
 }
 
 object LzListPlayground {
   def main(args: Array[String]): Unit = {
     val naturals = LzList.generate(1)(n => n + 1) //INFINITE list of natural numbers
+//    println(naturals.head) // 1
+//    println(naturals.tail.head) // 2
+//    println(naturals.tail.tail.head) // 3
+//
+//    val first50k = naturals.take(50000)
+//    first50k.foreach(println)
+//    val first50kList = first50k.toList
+//    println(first50kList)
+
+    //test classics
+    println(naturals.map(_ *2).takeAsList(100))
+    println(naturals.flatMap(x => LzList(x, x + 1)).takeAsList(100))
+    println(naturals.filter(_ < 10).takeAsList(9))
+    // println(naturals.filter(_ < 10).takeAsList(10)) // crash with SO or infinite rec
+
+    val combinationsLazy = for {
+      number <- LzList(1,2,3)
+      string <- LzList("black", "white")
+    } yield s"$number-$string"
+    println(combinationsLazy.toList)
   }
 }
